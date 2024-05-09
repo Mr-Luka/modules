e27fc2a2d24850cf9f10bde2fe4c021aaefd4282
Luka <luka.luka.luka93@gmail.com>
2024-05-09 12:39:21 -0700

    imported * as everything from

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 import { returnHi as sayHi, last, middle } from "./utils.js";
 
-import randomName from "./wes.js";
-console.log(randomName)
-
+import * as everything from "./wes.js"
+console.log(everything)
 const name = "wes";
 console.log(sayHi(name))
 console.log("It's working");
--- wes.js
+++ wes.js
@@ -4,3 +4,10 @@ const person = {
 }
 
 export default person;
+
+export const dog = "Snickers";
+export const food = "pizza";
+
+export function eat() {
+    console.log("chomp chomp")
+};
